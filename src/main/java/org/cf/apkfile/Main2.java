fedb2ba55821ad9a29b63f337480237d59e75092
Caleb Fenton <calebjfenton@gmail.com>
2019-11-06 17:45:30 -0800

    Progress commit before 2.x rewrite

---
+++ src/main/java/org/cf/apkfile/Main2.java
@@ -0,0 +1,14 @@
+package org.cf.apkfile;
+
+import net.lingala.zip4j.core.ZipFile;
+import net.lingala.zip4j.model.FileHeader;
+
+public class Main2 {
+    public static void main(String[] args) throws Exception {
+        ZipFile zipFile = new ZipFile(args[0]);
+        System.out.println("Is valid " + zipFile.isValidZipFile());
+        for (Object header : zipFile.getFileHeaders()) {
+            System.out.println("Found header: " + ((FileHeader) header).getFileName());
+        }
+    }
+}
